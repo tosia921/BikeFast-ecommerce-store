6537153d3c87111ceca9ccb1e4956694779ee378
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-03-11 18:17:58 +0000

    working on backend code

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
+
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 dotenv.config();
@@ -11,11 +13,14 @@ connectDB();
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('api is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
