b268a7e99a4672a835cbf7e0677261ced1037ec0
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-03-11 21:05:02 +0000

    created back end functionality for authorization and authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,29 +1,10 @@
-import asyncHandler from 'express-async-handler'
-import User from '../models/userModel.js'
+import express from 'express'
+const router = express.Router()
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
-// @desc     Auth user & get token
-// @route    POST /api/users/login
-// @access   Public
+router.route('/').post(registerUser)
+router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
-const authUser = asyncHandler(async(req,res) => {
-    const {email, password} = req.body
-
-    const user = await User.findOne({email})
-
-    if(user && (await user.matchPassword(password))) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: null
-        })
-    } else {
-        res.status(401)
-        throw new Error('Invalid email or password')
-    }
-})
-
-export {
-    authUser
-}
+export default router
