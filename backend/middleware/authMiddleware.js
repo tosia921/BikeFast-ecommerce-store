4683ae0e1bd27e2cb93190463c4296e43f2f5e81
Kamila Tarnożek <tomaszposiadala@gmail.com>
2021-03-12 11:01:48 +0000

    fixed problem with backend not reading password from frontend properly on register

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,31 +1,33 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
-import User from '../models/userModel.js';
+import User from '../models/userModel.js'
 
-const protect = asyncHandler (async (req, res, next) => {
-    let token
+const protect = asyncHandler(async (req, res, next) => {
+  let token
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try {
-            token = req.headers.authorization.split(' ')[1]
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
 
-            req.user = await (await User.findById(decoded.id)).isSelected('-password')
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            next()
-        } catch (error) {
-            console.error(error)
-            res.status(401)
-            throw new Error('Not authorized, token failed')
-        }
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not authorized, token failed')
     }
+  }
 
-    if(!token) {
-        res.status(401)
-        throw new Error('Not authorized, no token')
-    }
-
-    next()
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
 })
 
 export { protect }
