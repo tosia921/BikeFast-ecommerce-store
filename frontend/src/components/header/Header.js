107d4839bcd0d3829f6f386da41dc7a22408c7fe
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-03-12 00:18:33 +0000

    added logout action to userActions, changed header component, so it checks whatever user is logged in or logged out, and accordingly displays either profile name as link to profle settings and logout button, or link to sign in page

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './Header.scss';
 import { FiShoppingCart } from 'react-icons/fi';
 import { BiUser } from 'react-icons/bi';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../redux/actions/userActions'
 
-import { NavLink } from 'react-router-dom';;
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header className='header'>
             <div className='header--logo--container'>
@@ -18,10 +30,25 @@ const Header = () => {
                 <NavLink className='nav--link' activeStyle={{color: "#ef8216"}} to='/contact' exact>CONTACT</NavLink>
             </nav>
             <div className='login-shopcart--container'>
-                <BiUser className='user-icon'/>
-                <NavLink className='login--cart--link' activeStyle={{color: "#ef8216"}} to='/signin' exact>SIGN IN</NavLink>
-                <FiShoppingCart className='user-icon'/>
-                <NavLink className='login--cart--link' activeStyle={{color: "#ef8216"}} to='/cart' exact>CART</NavLink>
+            {userInfo ? (
+                <div className='logged-in-user-container'>
+                    <div className='user-link-and-icon'>
+                        <BiUser className='user-loggedin-icon'/>
+                        <NavLink className='link-to-profile' activeStyle={{color: "#ef8216"}} to='/profile'>{userInfo.name}</NavLink>
+                    </div>
+                    <p onClick={logoutHandler}>Logout</p>
+                </div>
+                ) : (
+                <div className='links-container'>
+                    <BiUser className='user-icon'/>
+                    <NavLink className='login--cart--link' activeStyle={{color: "#ef8216"}} to='/login' exact>SIGN IN</NavLink>
+                </div>
+            )}
+                
+                <div className='links-container'> 
+                    <FiShoppingCart className='user-icon'/>
+                    <NavLink className='login--cart--link' activeStyle={{color: "#ef8216"}} to='/cart' exact>CART</NavLink>
+                </div>
             </div>
         </header>
     )
